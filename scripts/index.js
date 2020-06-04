e50949c43b67c3523feec788ca9f6eb36daa116e
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-06-03 22:00:07 -0500

    chore(build): move build devDeps to scripts dir

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,15 @@ function main() {
   const scriptsDir = __dirname;
   const rootDir = path.join(scriptsDir, '..');
   const scriptsBuildDir = path.join(scriptsDir, 'build');
+  const scriptsNodeModulesDir = path.join(scriptsDir, 'node_modules');
   const scriptsBuildJs = path.join(scriptsBuildDir, 'build.js');
   const args = process.argv.slice(2);
+
+  if (args.includes('--prepare') || !fs.existsSync(scriptsNodeModulesDir)) {
+    console.log('💾  installing build scripts node_modules');
+    const execa = require('execa');
+    execa.sync('npm', ['i'], { cwd: scriptsDir });
+  }
 
   if (args.includes('--prepare') || !fs.existsSync(scriptsBuildJs)) {
     // ensure we've transpiled the build scripts first
@@ -15,7 +22,7 @@ function main() {
     const tscPath = path.join(rootDir, 'node_modules', '.bin', 'tsc');
     const tsconfig = path.join(scriptsDir, 'tsconfig.json');
     const execa = require('execa');
-    execa.sync(tscPath, ['-p', tsconfig]);
+    execa.sync(tscPath, ['-p', tsconfig], { cwd: scriptsDir });
   }
 
   const build = require(scriptsBuildJs);
